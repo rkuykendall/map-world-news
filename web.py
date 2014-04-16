e51aae062d2b380a9793c83e2887aa2c595d5c2a
Robert Kuykendall <robert@rkuykendall.com>
2014-04-15 22:41:36 -0400

    Rearranging static files.

--- web.py
+++ web.py
@@ -7,7 +7,22 @@ app.debug = True
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return app.send_static_file('index.html')
+
+@app.route('/css/<file>')
+def css(file=None):
+    return app.send_static_file('css/'+file)
+
+@app.route('/js/<file>')
+def js(file=None):
+    return app.send_static_file('js/'+file)
+
+@app.route('/json/<file>')
+def json(file=None):
+    return app.send_static_file('json/'+file)
+
+
 
 # Hack so people can work on the HTML/JS without python
 @app.route('/world-50m.json')
@@ -16,9 +31,9 @@ def data():
     
 @app.route('/articles.json')
 def articles():
-    feed = Feed(file='data/2014-04-05_16-54.atom')
+    feed = Feed('data/2014-04-05_16-54.atom')
     json_string = ""
     
     for article in feed.articles:
-        json_string += article.to_json()
+        json_string += feed.articles[article].to_json()
     return json_string
