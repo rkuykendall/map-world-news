b36c3f18653f7a476859e9209d898c49928c97bc
Robert Kuykendall <robert@rkuykendall.com>
2014-08-13 00:24:10 -0400

    Sucess publishing to S3.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,52 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    gzip = require('gulp-gzip'),
+    awspublish = require('gulp-awspublish'),
+    rename = require('gulp-rename'),
+    fs = require('fs');
 
-gulp.task('minify', function () {
-    gulp.src('iris/static/js/main.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('iris/static/build'))
+gulp.task('deploy', function () {
+    var aws = JSON.parse(fs.readFileSync('aws.json'));
+    var publisher = awspublish.create({ key: process.env.KEY,  secret: process.env.SECRET, bucket: 'map-world-news' });
+
+    var headers = {
+        'Cache-Control': 'max-age=315360000, no-transform, public'
+        // ...
+    };
+
+    gulp.src('./iris/static/js/*')
+
+        // gzip, Set Content-Encoding headers and add .gz extension
+        // .pipe(awspublish.gzip({ ext: '.gz' }))
+
+        .pipe(rename(function (path) {
+            path.dirname += '/static/js';
+        }))
+
+        // publisher will add Content-Length, Content-Type and  headers specified above
+        // If not specified it will set x-amz-acl to public-read by default
+        .pipe(publisher.publish(headers))
+
+        // create a cache file to speed up consecutive uploads
+        .pipe(publisher.cache())
+
+         // print upload updates to console
+        .pipe(awspublish.reporter());
+
+    gulp.src('./iris/static/css/*')
+        .pipe(rename(function (path) {
+            path.dirname += '/static/css';
+        }))
+        .pipe(publisher.publish(headers))
+        .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
+
+    gulp.src('./iris/static/json/*')
+        .pipe(rename(function (path) {
+            path.dirname += '/static/json';
+        }))
+        .pipe(publisher.publish(headers))
+        .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
+
 });
