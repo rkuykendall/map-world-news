833ad5e6e3debd730a8ba5f1a04717c5079cd581
Robert Kuykendall <robert@rkuykendall.com>
2015-08-16 19:56:41 -0400

    Fix test environment in feed test.

--- tests/feed_test.py
+++ tests/feed_test.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 # Set enviornment before importing any database classes
-os.environ["CONFIG_PATH"] = "database.TestingConfig"
+os.environ["CONFIG_PATH"] = "iris.config.TestingConfig"
 from iris.feed import Feed
 
 
