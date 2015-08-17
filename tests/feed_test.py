642a88bea80196665adbf49009c90dd5d7fd575f
Robert Kuykendall <robert@rkuykendall.com>
2015-08-16 20:03:35 -0400

    Feedzilla is dead.

--- tests/feed_test.py
+++ tests/feed_test.py
@@ -7,17 +7,6 @@ from iris.feed import Feed
 
 
 class TestFeed(unittest.TestCase):
-    def test_from_feed(self):
-        # # Commented out because search doesn't always have 10 things to
-        # # return
-        # q="flight"
-        # url="http://api.feedzilla.com/v1/categories/27/articles/
-        # search.atom?q="+q+"&count=10"
-        url = (
-            "http://api.feedzilla.com/v1/categories/16/articles.atom?count=10")
-        feed = Feed(url)
-        self.assertEqual(len(feed.articles), 10)
-
     def test_from_file(self):
         feed = Feed('tests/data/2014-04-05_16-54.atom')
         self.assertEqual(len(feed.articles), 89)
@@ -26,10 +15,3 @@ class TestFeed(unittest.TestCase):
         feed = Feed('tests/data/2014-04-05_16-54.atom')
         feed.add_feed('tests/data/2014-04-05_16-54.atom')
         self.assertEqual(len(feed.articles), 89)
-
-    def test_cray(self):
-        url = (
-            "http://api.feedzilla.com/v1/categories/16/articles.atom?count=10")
-        feed = Feed('tests/data/2014-04-05_16-54.atom')
-        feed.add_feed(url)
-        self.assertEqual(len(feed.articles), 99)
