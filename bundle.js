40db78cd2b4fb980fbcbfe0d733ae455a6f9ad4a
Travis-CI <robert@rkuykendall.com>
2015-08-31 03:42:49 +0000

    Update 2015-08-31T03:42:48.696Z

--- bundle.js
+++ bundle.js
@@ -113,7 +113,7 @@
 	    .on('click', countryClicked);
 
 	var g = svg.append('g');
-	d3.json('/json/countries.topo.json', function (error, us) {
+	d3.json('countries.topo.json', function (error, us) {
 	    g.append('g')
 	        .attr('id', 'countries')
 	        .selectAll('path')
