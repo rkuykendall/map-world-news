c37bb6f17b9254773b484721d95059095b2ef28a
Robert Kuykendall <robert@rkuykendall.com>
2015-08-31 00:30:14 -0400

    Working less compiler.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    entry: './web/main.js',
+    entry: './web/app.es6',
     output: {
         path: require('path').resolve('./web'),
         filename: 'bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' }
+            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }
         ]
     }
 };
