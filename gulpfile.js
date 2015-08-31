402c01de7b61a6d42a664daf7d1c8ad374c62291
Robert Kuykendall <robert@rkuykendall.com>
2015-08-30 23:37:11 -0400

    Acually fixed deploy.

--- gulpfile.js
+++ gulpfile.js
@@ -39,8 +39,7 @@ gulp.task('deploy', function() {
   webpack(webpackConfig, function(err, stats) {
       if (err) throw new gutil.PluginError('webpack', err);
       gutil.log('[webpack]', stats.toString());
+      return gulp.src('./web/**/*')
+        .pipe(ghPages({ force: true }));
   });
-
-  return gulp.src('./web/**/*')
-    .pipe(ghPages({ force: true }));
 });
