69f9812f93c18ed56a2ac9404462261fab53c0b7
Robert Kuykendall <robert@rkuykendall.com>
2020-03-09 21:14:12 -0400

    Fix CORS?

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from flask import Flask
-from flask.ext.cors import CORS
+from flask_cors import CORS
 from views import api_blueprint
 
 
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(api_blueprint)
 
 
-# Set enviornment variables from the database file
+# Set environment variables from the database file
 config_path = os.environ.get("CONFIG_PATH", "api.config.DevelopmentConfig")
 app.config.from_object(config_path)
 cors = CORS(app)
